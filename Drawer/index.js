7b93770398524ba68aed7977fe0b22791055389c
Ashal aka JOKER <ahastudio@gmail.com>
2017-10-09 07:07:56 +0900

    PropTypes
    PropTypes has been moved to a separate package.
    
    - https://fb.me/migrating-from-react-proptypes
    - https://reactjs.org/blog/2017/04/07/react-v15.5.0.html

--- Drawer/index.js
+++ Drawer/index.js
@@ -2,7 +2,8 @@
  * Created by TinySymphony on 2017-02-14.
  */
 
-import React, {PropTypes, Component} from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
